9348bf636664022c6b9b2e13c1653a76184b0cc8
wjswhdgus123 <wjswhdgus123@naver.com>
2023-07-23 00:20:11 +0900

    20230723

---
+++ 20230722/Number.py
@@ -0,0 +1,15 @@
+
+a = 123
+b = -123
+c = 0
+
+print (a, b, c)
+
+
+food = "python's favorite food is peer"
+food2 = '''python's favorite food is peer'''
+
+print (food)
+print(food2)
+
+print(food[0:3])
